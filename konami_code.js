28379e275e257752d065b022c71ae955198b721c
Robert D Gauthier <rgauthie@gmail.com>
2018-03-21 14:58:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  document.body.addEventListener(e);
+  document.body.addEventListener(onKeyDownHandler);
   function onKeyDownHandler(e) {
     console.log(`e.detail: ${e.detail} e.which: ${e.which}`)
     const key = parseInt(e.detail || e.which);
